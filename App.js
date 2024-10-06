7032dc3bb6fe2fb757009dbabdd7202681dc89a7
Danny Alvarez <dany.alvarez@uned.cr>
2024-10-06 12:24:38 -0500

    finished updates. All handled

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {isAuthenticated ? (
             <Stack.Screen name="AppDrawer">
-                {(props) => <AppDrawer {...props} logout={handleLogout} />}
+                {(props) => <AppDrawer {...props} logout={handleLogout} />} 
             </Stack.Screen>
         ) : (
           <>
